690179abb6ddc3d41fedbce2534e32c7d5e41e9d
Daniel <dortiz.ven94@gmail.com>
2015-07-31 09:21:39 -0500

    Buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,7 +14,7 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(function(quizes) {
+  models.Quiz.findAll({where:["pregunta LIKE ?", search], order:"pregunta ASC"}).then(function(quizes) {
     res.render('quizes/index', { quizes: quizes});
   }).catch(function(error) {next(error);})
 };
